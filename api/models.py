8440872836c2c4a55503af747523ea7c7f9187c8
smbrine <smbrine@yandex.ru>
2024-01-18 00:13:12 +0300

    Run black

--- api/models.py
+++ api/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
+
 class Record(models.Model):
-    id=models.CharField(max_length=16)
-    filename=models.FileField(upload_to=None, max_length=100)
-    title=models.CharField(max_length=16)
-    paragraph1=models.TextField(blank=True)
-    paragraph2=models.TextField(blank=True)
-    
+    id = models.CharField(max_length=16)
+    filename = models.FileField(upload_to=None, max_length=100)
+    title = models.CharField(max_length=16)
+    paragraph1 = models.TextField(blank=True)
+    paragraph2 = models.TextField(blank=True)
+
+
 class GeoJSON(models.Model):
-    id=models.CharField(max_length=16)
-    tag=''
-    obj=''
+    id = models.CharField(max_length=16)
+    tag = ""
+    obj = ""
